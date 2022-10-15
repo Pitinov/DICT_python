5f0347174975eb27168a2fca5330189c1bbd43a0
Ім'я корPitinov Yaroslav PavlovichÐ <y.p.pitinov@student.khai.edu>
2022-10-15 15:06:12 +0300

    Hangman5

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,7 +6,7 @@ WORLDS = ["python", "java", "php", "javascript"]
 word = random.choice(WORLDS)
 remember_letters = []
 all_letters = list(set(word))
-words = ''.join([i if i in remember_letters else "_"for i in word])
+words = ''.join([i if i in remember_letters else "_" for i in word])
 print(words)
 life = 8
 while life > 0:
@@ -14,10 +14,14 @@ while life > 0:
     if letter in word and letter not in remember_letters:
         remember_letters.append(letter)
         all_letters.remove(letter)
-    else:
+    if letter not in word:
         life -= 1
+        print("That letter doesnt`s appear un the word")
+    if letter in remember_letters:
+        print("Cool")
     words = ''.join([i if i in remember_letters else "_" for i in word])
     print(words)
     if len(all_letters) == 0:
         break
+
 print("thanks for playing\nWe'll see how well you did in the next stage")
