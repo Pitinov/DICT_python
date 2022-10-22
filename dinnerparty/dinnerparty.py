0f80f904c0ede537e579aadf34770526bbb5ea84
Ім'я корPitinov Yaroslav PavlovichÐ <y.p.pitinov@student.khai.edu>
2022-10-22 13:18:00 +0300

    dinnerparty2

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,4 +1,4 @@
-number = int(input("Enter the number of friends joining (including you):\n >" ))
+number = int(input("Enter the number of friends joining (including you):\n >"))
 if number < 1:
     print("No one is joining for the party")
     exit()
@@ -7,4 +7,8 @@ print("Enter the name of every friend (including you), each on a new line:")
 for _ in range(number):
     name = input(">")
     payment[name] = 0
+money = int(input("Enter the total amount:\n"))
+a = round(money/number, 2)
+for name in payment:
+    payment[name] = a
 print(payment)
