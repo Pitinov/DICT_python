136bd607d3c7bf942de0dc89e8d2b98213ac5c26
Ім'я корPitinov Yaroslav PavlovichÐ <y.p.pitinov@student.khai.edu>
2022-10-22 14:06:11 +0300

    dinnerparty3

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,3 +1,5 @@
+import random
+yes_no = {'yes': True, 'no': False}
 number = int(input("Enter the number of friends joining (including you):\n >"))
 if number < 1:
     print("No one is joining for the party")
@@ -8,7 +10,14 @@ for _ in range(number):
     name = input(">")
     payment[name] = 0
 money = int(input("Enter the total amount:\n"))
+print("Do you want to use the 'Who is lucky?' feature? Write Yes/No:")
+lucky = yes_no[input('>').lower()]
+lucky_name = ''
+if lucky:
+    lucky_name = random.choice(list(payment.keys()))
+    print(lucky_name, "is the lucky one!")
+else:
+    print("No one is going to be lucky")
 a = round(money/number, 2)
 for name in payment:
     payment[name] = a
-print(payment)
