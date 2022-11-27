aae4931414c2207b145eea44b7a6e6afcd676c3b
Ім'я корPitinov Yaroslav PavlovichÐ <y.p.pitinov@student.khai.edu>
2022-11-27 20:34:06 +0300

    feature/matrixprocessing2

---
+++ matrixprocessing/matrixprocessing.py
@@ -0,0 +1,29 @@
+import random
+your_input = input("Enter size of matrix:\n> ")
+cons = input("Enter constant:\n> ")
+n = int(your_input[0])
+m = int(your_input[2])
+a = [[random.randrange(0, 10) for w in range(m)] for e in range(n)]
+
+print(your_input)
+
+p = 0
+for i in range(n):
+    for x in a[p]:
+        print(x, end="  ")
+    print()
+    p += 1
+
+print(cons)
+print("Result:")
+print()
+
+p = 0
+for i in range(n):
+    k = 0
+    for x in a[p]:
+        x *= int(cons)
+        print(x, end="  ")
+        k += 1
+    print()
+    p += 1
