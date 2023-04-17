fa3af04ab6114f5ff0596776668384ef7512bce8
Ім'я корPitinov Yaroslav PavlovichÐ <y.p.pitinov@student.khai.edu>
2023-04-17 14:28:40 +0300

    feature/rock-paper-scissors4

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,21 +1,44 @@
 import random
 
-options = ['rock', 'paper', 'scissors']
+# зчитати рейтинг з файлу rating.txt
+with open("rating.txt", "r") as file:
+    rating = dict(line.strip().split() for line in file)
+
+# запустити гру
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+# отримати початковий бал користувача
+user_score = int(rating.get(name, 0))
 
 while True:
     user_input = input()
+
     if user_input == "!exit":
         print("Bye!")
         break
-    elif user_input not in options:
-        print("Invalid input")
-    else:
-        computer_choice = random.choice(options)
-        if user_input == computer_choice:
-            print(f"There is a draw ({computer_choice})")
-        elif user_input == "rock" and computer_choice == "scissors" \
-                or user_input == "paper" and computer_choice == "rock" \
-                or user_input == "scissors" and computer_choice == "paper":
-            print(f"Well done. The computer chose {computer_choice} and failed")
+    elif user_input == "!rating":
+        print(f"Your rating: {user_score}")
+    elif user_input in ["rock", "paper", "scissors"]:
+        options = ["rock", "paper", "scissors"]
+        computer_option = random.choice(options)
+
+        if user_input == computer_option:
+            print(f"There is a draw ({computer_option})")
+            user_score += 50
+        elif (user_input == "rock" and computer_option == "scissors") or \
+             (user_input == "paper" and computer_option == "rock") or \
+             (user_input == "scissors" and computer_option == "paper"):
+            print(f"Well done. The computer chose {computer_option} and failed")
+            user_score += 100
         else:
-            print(f"Sorry, but the computer chose {computer_choice}")
+            print(f"Sorry, but the computer chose {computer_option}")
+    else:
+        print("Invalid input")
+
+# зберегти оновлений рейтинг у файл rating.txt
+with open("rating.txt", "w") as file:
+    for player, score in rating.items():
+        if player == name:
+            score = user_score
+        file.write(f"{player} {score}\n")
