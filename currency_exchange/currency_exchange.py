25dcd3f479e6f1943aa1567c944ce3940b59b5c1
Pitinov <lololos.nol@gmail.com>
2023-05-15 11:22:50 +0300

    feature/currency_exchange 2-stage

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,3 +1,18 @@
-coins = int(input("Please, enter the number of mycoins you have:\n>"))
-rate = int(input("Please, enter the exchange rate:\n>"))
-print("The total amount of dollars:", coins * rate)
+print("--------------------------------------------------------------------------")
+money = float(input("Please, enter the number of money you have:"))
+r_ars = 0.3
+r_hnl = 1.2
+r_aud = 28
+r_mad = 56
+
+ARS = money*r_ars
+HNL = money*r_hnl
+AUD = money*r_aud
+MAD = money*r_mad
+
+print("I will get:",round(ARS,2),print("ARS from the sale of",money,"money"))
+print("I will get:",round(HNL,2),print("HNL from the sale of",money,"money"))
+print("I will get:",round(AUD,2),print("AUD from the sale of",money,"money"))
+print("I will get:",round(MAD,2),print("MAD from the sale of",money,"money"))
+print("--------------------------------------------------------------------------")
+print("Have a nice day")
